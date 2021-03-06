69588663bff5b41de3f554da3f2a9ab409c3a758
Yusuf Adanur <y.adanur@gmail.com>
2021-03-06 13:58:55 -0800

    this is completed

--- javascript-this/student.js
+++ javascript-this/student.js
@@ -1,1 +1,12 @@
 /* exported student */
+var student= {
+  firstName: 'Yusuf',
+  lastName: 'Adanur',
+  subject: 'JavaScript',
+  getFullName: function (){
+    return (this.firstName+" "+ this.lastName);
+  },
+  getIntroduction: function(){
+    return ("Hello, my name is "+ this.firstName+" "+this.lastName+" and I am studying "+ this.subject+".");
+  }
+}
