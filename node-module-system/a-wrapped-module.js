d5eb8e2387d0a032dfb990a75d50705811c26109
Yusuf Adanur <y.adanur@gmail.com>
2021-04-24 14:41:07 -0700

    completed wrapped module

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,10 @@
+console.log(typeof(exports));
+console.log(exports);
+console.log(typeof(require));
+console.log(require);
+console.log(typeof(module));
+console.log(module);
+console.log(typeof(__filename));
+console.log(__filename);
+console.log(typeof(__dirname));
+console.log(__dirname);
