1b4d8cc48a9b73fa37329b2e3610bb6dce8519b7
Yusuf Adanur <y.adanur@gmail.com>
2021-05-14 17:01:38 -0700

    not done yet

---
+++ express-get-json/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const strGrades = JSON.stringify(grades);
+
+app.get('/api/grades', function (req, res) {
+  res.json(strGrades);
+  res.send(strGrades);
+});
+
+app.listen(3000);
