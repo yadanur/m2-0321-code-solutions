ffb82ede8c96f1d871912ca2ab52ef36ba61e76a
Yusuf Adanur <y.adanur@gmail.com>
2021-05-22 23:58:27 +0000

    sql aggregate funstions

---
+++ sql-aggregates/exercise/movie-maniacs.sql
@@ -0,0 +1,8 @@
+select  "customers"."customerId",
+        "customers"."firstName",
+        "customers"."lastName",
+        sum("payments"."amount") as "totalSpent"
+from "customers"
+join "payments" using ("customerId")
+group by "customers"."customerId"
+order by "totalSpent" desc;
