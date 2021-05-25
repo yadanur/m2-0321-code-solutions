f8ef7cb4f0e67a5da4e0a65e784b4cddc6c4d888
Yusuf Adanur <y.adanur@gmail.com>
2021-05-25 18:01:07 +0000

    completed post

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const app = express();
 
-// let nextId = 2;
-const grades = {
-  1: {
-    name: 'Brendan Eich',
-    course: 'JavaScript',
-    score: 100
-  }
-};
+let nextId = 1;
+const grades = {};
 
 app.get('/api/grades', function (req, res) {
   const gradesArray = [];
@@ -18,24 +12,14 @@ app.get('/api/grades', function (req, res) {
   res.json(gradesArray);
 });
 
-app.use(express.json());
+app.use(express.json()); // app parses the json request body
 
 app.post('/api/grades', function (req, res) {
-  const gradesArray = [];
-  for (const property in grades) {
-    gradesArray.push(grades[property]);
-  }
-  /*
-  const addName = req.params.name;
-  const addCourse = req.params.course;
-  const addScore = req.params.score;
-  gradesArray[nextId][name] = addName;
-  gradesArray[nextId][course] = addCourse;
-  gradesArray[nextId][score] = addScore;
+  const requestObject = req.body;
+  requestObject.id = nextId;
+  grades[nextId] = requestObject;
   nextId++;
-  */
-  res.sendStatus();
+  res.status(201).json(requestObject);
 });
 
-// eslint-disable-next-line no-console
-app.listen(3000, () => console.log('listening 3000'));
+app.listen(3000); // app.listen(3000, () => console.log('listening 3000'));
